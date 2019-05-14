1205295968beffe3a1f31c48bb064e18de8bbba7
tetsuya maekawa <maekawa@rootship.co.jp>
2019-05-14 11:42:53 +0900

    test create

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
+
+	// SDK追加
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
@@ -16,6 +19,16 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
+	// LINE bot instanceの作成
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("templates/*.tmpl.html")
@@ -23,6 +36,29 @@ func main() {
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	// Line Messaging API用　Routing設定
+	router.POST("/callback", func(c *gin.Context) {
+
+		events, err := bot.ParseRequest(c.Request)
+
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				log.Print(err)
+			}
+			return
+		}
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
 	})
 
 	router.Run(":" + port)
