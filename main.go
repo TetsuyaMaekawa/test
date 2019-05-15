55101e22d7f530c8a03b5785bb74d4c8fc416167
tetsuya maekawa <maekawa@rootship.co.jp>
2019-05-15 11:59:05 +0900

    res userID

--- main.go
+++ main.go
@@ -59,28 +59,25 @@ func main() {
 
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
- 				if  getResMessage(message.Text) == "情報" {
-					userId := event.Source.UserID
-					userName := bot.GetProfile(userId)
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("あなたのユーザーIDは：" + userId + "\n" + "あなたのユーザ名は：" + userName)).Do(); err != nil {
+				// if getResMessage(message.Text) == "情報" {
+				if message.Text == "情報" {
+					userID := event.Source.UserID
+					// profile := bot.GetProfile(userId)
+
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("あなたのユーザーIDは："+userID+"\n"+"あなたのユーザ名は：")).Do(); err != nil {
 						log.Print(err)
-					l
 					}
 				}
 			}
-
-
 
 			// // メッセージが送信されてきた時の振る舞い
 			// if event.Type == linebot.EventTypeMessage {
 			// 	if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("そうですね。")).Do(); err != nil {
 			// 		log.Print(err)
-				}
-			}
+			// 	}
+			// }
 		}
 	})
 
 	router.Run(":" + port)
 }
-
-
